e1bb8f9220076151c0fe53528f9bc95cc3c85071
George <lets.devel0pme@gmail.com>
2026-01-06 17:05:14 -0300

    manipulacion del dom practice

---
+++ main.js
@@ -0,0 +1,46 @@
+
+
+console.log('DOM Tutorial');
+
+// seleccionar titulo
+let title2 = document.getElementById("title"); //forma antigua
+const title = document.querySelector("#title"); //forma moderna
+
+
+//estudiar eventos en dom y javascript
+
+
+title.addEventListener("click", () => {
+
+    console.log("click en el titulo");
+
+    title.innerHTML = "Manipulacion del DOM, Modelo del documento";
+    //title.style.color = "grey";
+    //title.style.fontSize = "5rem";
+
+    //title.classList.add("big__title");
+    title.classList.toggle("big__title");
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
